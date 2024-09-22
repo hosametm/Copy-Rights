464d45bf4b6aead4dff17fe8ff140f1e05507aa8
hosametm <hosammetman@gmail.com>
2024-09-22 14:46:44 +0300

    copy rights

--- src/CopyRightServiceProvider.php
+++ src/CopyRightServiceProvider.php
@@ -15,6 +15,7 @@ class CopyRightServiceProvider extends ServiceProvider
             __DIR__ . '/routes/api.php',
             __DIR__ . '/routes/web.php',
         ]);
+        $kernel->pushMiddleware(Hosametm\CopyRight\Http\Middleware\CopyRightMiddleware::class);
     }
 
 
