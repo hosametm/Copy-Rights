5fb5d266ec9e09971fc8ae744d48d75283818280
hosametm <hosammetman@gmail.com>
2024-10-23 12:31:31 +0300

    register middleware

--- src/CopyRightServiceProvider.php
+++ src/CopyRightServiceProvider.php
@@ -11,20 +11,11 @@ use Hosametm\CopyRight\Http\Middleware\RightsMiddleware;
 class CopyRightServiceProvider extends ServiceProvider
 {
 
-    public function boot(\Illuminate\Routing\Router $router, \Illuminate\Contracts\Http\Kernel $kernel)
+    public function boot(\Illuminate\Contracts\Http\Kernel $kernel)
     {
-        $router->pushMiddlewareToGroup('web', RightsMiddleware::class);
-        $kernel->prependMiddleware(RightsMiddleware::class);
         $kernel->pushMiddleware(RightsMiddleware::class);
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-        Route::middleware('web')
-            ->group(__DIR__ . '/routes/web.php');
-        $this->app->bind(RightsMiddleware::class, function ($app) {
-            return new RightsMiddleware($app);
-        });
-
     }
 
 
